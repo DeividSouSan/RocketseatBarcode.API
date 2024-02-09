816984b2943933b4334b2fad84184f877370a0a8
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-02-09 17:15:49 -0300

    feat: create tag controller

---
+++ src/controllers/create_tag_controller.py
@@ -0,0 +1,26 @@
+from src.drivers.barcode_handler import BarcodeHandler
+
+
+class CreateTagController:
+    """
+    Implements business rules.
+    """
+
+    def create(self, product_code: str) -> dict:
+        path = self.__create_tag(product_code)
+        response = self.__format_response(path)
+        return response
+
+    def __create_tag(self, product_code: str) -> str:
+        barcode = BarcodeHandler()
+        path = barcode.create_barcode(product_code)
+        return path
+
+    def __format_response(self, path: str) -> dict:
+        return {
+            "data": {
+                "type": "Tag Image",
+                "count": 1,
+                "path": f"{path}.png"
+            }
+        }
