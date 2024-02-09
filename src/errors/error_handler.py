c23379033102d3fed297226bf1f2c081045bb6be
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-02-09 18:30:02 -0300

    feat: handling new UnprocessableEntityError error

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,7 +1,19 @@
+from src.errors.error_types.unprocessable_entity import HttpUnprocessableEntityError
 from src.views.http_types.http_response import HttpResponse
 
 
 def handle_errors(error: Exception) -> HttpResponse:
+    if isinstance(error, HttpUnprocessableEntityError):
+        return HttpResponse(
+            {
+                "error": [{
+                    "title": error.name,
+                    "detail": error.message
+                }]
+            },
+            error.status_code
+        )
+
     return HttpResponse(
         {
             "error": [{
